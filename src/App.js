138e16573c5662114b7539057155e17d43b02f8c
vijay <Vijay.PV@expeditors.com>
2021-09-21 12:44:15 +0530

    commit on 21/09/2021

--- src/App.js
+++ src/App.js
@@ -1,4 +1,35 @@
+import github from "./db";
+import { useEffect } from "react";
+
+
 function App() {
+
+  useEffect(() => {
+    const githubQuery = {
+      query: `
+      {
+        viewer {
+          name
+        }
+      }
+    ` 
+    };
+
+    fetch(github.baseURL, {
+      method: "POST",
+      headers: github.headers,
+      body: JSON.stringify(githubQuery)
+    }) 
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) =>{
+      console.error(error);
+    });
+  });
+
+  
   return (
     <div className="App container mt-5">
       <h1 className="text-primary"><i className="bi bi-diagram-2-fill"></i> Repos </h1>
